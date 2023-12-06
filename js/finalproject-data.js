ddcb57145c0922b6f6878a5944882c802bfe23ad
mikaniamh <mikaniamh@gmail.com>
2023-12-06 00:57:32 -0800

    final clean up, keyboard init, design tweaks

--- js/finalproject-data.js
+++ js/finalproject-data.js
@@ -2,23 +2,18 @@
 // Mika Campbell Nishmura A01328907
 // Nov 29, 2023
 
-//TODO make object
-
-
 class WordPair {
     constructor(word, hint) {
         this.word = word;
         this.hint = hint;
     }
     }
-    
     WordPair.prototype.getHint = function() {
     return this.hint;
     }
     WordPair.prototype.getWord = function() {
     return this.word;
     }
-
 
 const word1 = new WordPair ("GOAT", "Think farm inhabitant.");
 const word2 = new WordPair ("STOMP", "Something you might do in frustration.");
@@ -27,4 +22,4 @@ const word4 = new WordPair ("MALLARD", "The male qauckers have green heads.");
 const word5 = new WordPair ("CARROT", "Peter Rabbit's root.");
 const word6 = new WordPair ("SUBMARINE", "The Nautilus, for one.");
 
-const words = [word1, word2, word3, word4, word5, word6];
+const wordPairs = [word1, word2, word3, word4, word5, word6];
--- js/finalproject-functions.js
+++ js/finalproject-functions.js
@@ -9,6 +9,7 @@ let wordLength = targetWord.length;
 let hintVisable = false;
 let correctGuesses = Array.from('-'.repeat(wordLength));
 let incorrectGuesses = [];
+let playStarted = false;
 
 // Play again button, restarts game
 function resetGame() {
@@ -17,7 +18,7 @@ function resetGame() {
 
 // returns random word from WordMap
 function getRandomPair() {
-    return words[Math.floor(Math.random() * words.length)];
+    return wordPairs[Math.floor(Math.random() * wordPairs.length)];
 }
 
 // displays the correct number of empty spaces in HTML for the selected word
@@ -29,6 +30,28 @@ function displayWord(){
         wordBlankHTMLOut +=`<p id="${i}" class="wordTile">${correctGuesses[i]}</p>`;
     }
     wordBlankElem.innerHTML=wordBlankHTMLOut;
+}
+
+function displayKeyboard(){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const keysElem = document.getElementById('virtual-keyboard');
+    let keysHTMLOut = ``;
+    let rowCount = 1;
+
+    for (i=0; i<alphabet.length;i++){
+        let keyValue = alphabet[i].toUpperCase();
+        keysHTMLOut += `<input id="${keyValue}" class="key" type="button" value="${keyValue}" onclick="input(value);" />`
+
+        if(rowCount==9){
+            keysHTMLOut += `<br>`;
+            rowCount=0;
+        }
+        rowCount++;
+    }
+    keysHTMLOut += `<input id="btnRestart" type="button" value="Restart" onclick="resetGame();" />`;
+    keysHTMLOut += `<input id="btnHint" type="button" value="?"/>`;
+
+    keysElem.innerHTML=keysHTMLOut;
 }
 
 // displays the corresponding hint
@@ -43,10 +66,15 @@ $(document).ready(function() {
 
     // Set up 
     displayWord(targetWord);
+    displayKeyboard();
+
+    // Changes image to create suspense after 2 seconds
 
     setTimeout(function() {
-        const hangmanImage = document.getElementById('hangman-image');
-        hangmanImage.src = `images/snowman-1.png`;
+        if (!playStarted){
+            const hangmanImage = document.getElementById('hangman-image');
+            hangmanImage.src = `images/snowman-1.png`;
+        }
     }, 2000);
 
     // Displays/Hides Hint
@@ -73,17 +101,18 @@ $(document).ready(function() {
 
 // Collects user input, greys buttons out once selected
 function input(letterGuessed) {
+    playStarted = true;
 
     checkGuess(letterGuessed);
 
     var buttonId = letterGuessed;
     $('input[id=' + buttonId + ']').prop('disabled', true);
-    $('input[id=' + buttonId + ']').css("background-color","grey");
+    $('input[id=' + buttonId + ']').css("background-color","#15295e");
     $('input[id=' + buttonId + ']').css("cursor", "not-allowed");
 
 }
 
-// Checks if letter is correct, adds letter to appropriate array
+// Checks if letter is correct, adds letter to correct/incorrect array
 function checkGuess(letterGuessed) {
     const letters = targetWord.split('');
     console.log(letters); 
@@ -123,22 +152,23 @@ function updateHangmanImage(guessCount) {
     hangmanImage.src = `images/snowman-${guessCount + 1}.png`;
 }
 
-        // Displays Play again pop-up after 3 seconds
+// Displays Play Again pop-up after 3 seconds
 function displayPlayAgainPopup(){
     setTimeout(function() {
         $('#popup').css('opacity', '1');
     }, 3000);
 }
 
-// Checks if game should end, max 7 guesses
+// Checks if game should end, max 7 guesses, handles end game if win or loss
 function checkGameEnd() {
     if (incorrectGuesses.length >= 7) {
         displayWord();
         $('input[id="incorrectGuesses"]').css("color","red");
         $('p[id="instructions"]').text("You Lost. Better luck next time...");
-        $('div[id="input-keybord-frame"]').css("pointer-events", "none");
+        $('input[class="key"]').css("pointer-events", "none");
         
         displayPlayAgainPopup();
+        $('#btnPlayAgain').css('opacity', 1);
     } else if (!correctGuesses.includes('-')) {
         displayWord();
         $('p[id="instructions"]').text("You Won! Well done!");
@@ -148,8 +178,10 @@ function checkGameEnd() {
         const hangmanImage = document.getElementById('hangman-image');
         hangmanImage.src = `images/snowman-win.png`;
 
-        $('div[id="input-keybord-frame"]').css("pointer-events", "none");
+        $('input[class="key"]').css("pointer-events", "none");
 
         displayPlayAgainPopup();
+
+        $('#btnPlayAgain').css('opacity', 1);
     }
 }
